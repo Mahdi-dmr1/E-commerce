05024644af6a59a6607ea838fa96081880ca74eb
Mahdi-dmr1 <damirchi9904@gmail.com>
2024-06-03 17:20:08 +0430

    new commit

--- slices/cartSlice.js
+++ slices/cartSlice.js
@@ -15,7 +15,10 @@ const cartSlice = createSlice({
 	initialState,
 	reducers: {
 		populateCart(state, action) {
-			if (sessionStorage.getItem("cartItems")) {
+			if (
+				sessionStorage.getItem("cartItems") &&
+				typeof window !== "undefined"
+			) {
 				console.log(sessionStorage.getItem("cartItems"));
 				cartAdapter.setAll(
 					state,
@@ -24,17 +27,19 @@ const cartSlice = createSlice({
 			}
 		},
 		addToCart(state, action) {
-			const productExist = state.entities[action.payload.id];
+			if (typeof window !== "undefined") {
+				const productExist = state.entities[action.payload.id];
 
-			if (productExist) {
-				state.entities[action.payload.id].cartQty += 1;
-			} else {
-				cartAdapter.addOne(state, action.payload);
-				toast.success("محصول به سبد خرید اضافه شد", {
-					position: "bottom-right",
-				});
+				if (productExist) {
+					state.entities[action.payload.id].cartQty += 1;
+				} else {
+					cartAdapter.addOne(state, action.payload);
+					toast.success("محصول به سبد خرید اضافه شد", {
+						position: "bottom-right",
+					});
+				}
+				sessionStorage.setItem("cartItems", JSON.stringify(state.entities));
 			}
-			sessionStorage.setItem("cartItems", JSON.stringify(state.entities));
 		},
 		getTotals(state, action) {
 			let { total, qty } = Object.values(state.entities).reduce(
@@ -56,22 +61,26 @@ const cartSlice = createSlice({
 			state.cartTotalAmount = total;
 		},
 		decreaseCart(state, action) {
-			const product = state.entities[action.payload.id];
+			if (typeof window !== "undefined") {
+				const product = state.entities[action.payload.id];
 
-			if (product.cartQty > 1) {
-				product.cartQty -= 1;
-			} else if (product.cartQty === 1) {
-				cartAdapter.removeOne(state, action.payload.id);
+				if (product.cartQty > 1) {
+					product.cartQty -= 1;
+				} else if (product.cartQty === 1) {
+					cartAdapter.removeOne(state, action.payload.id);
+				}
+				sessionStorage.setItem("cartItems", JSON.stringify(state.entities));
 			}
-			sessionStorage.setItem("cartItems", JSON.stringify(state.entities));
 		},
 		removeFromCart(state, action) {
-			cartAdapter.removeOne(state, action.payload.id);
-			sessionStorage.setItem("cartItems", JSON.stringify(state.entities));
+			if (typeof window !== "undefined") {
+				cartAdapter.removeOne(state, action.payload.id);
+				sessionStorage.setItem("cartItems", JSON.stringify(state.entities));
 
-			toast.error("محصول از سبد خرید حذف شد", {
-				position: "bottom-left",
-			});
+				toast.error("محصول از سبد خرید حذف شد", {
+					position: "bottom-left",
+				});
+			}
 		},
 	},
 });
