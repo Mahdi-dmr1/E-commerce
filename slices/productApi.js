fa47243630e6ba2ac8d1acb5513b5a004150a15f
Mahdi-dmr1 <damirchi9904@gmail.com>
2024-06-05 19:23:12 +0430

    latest-commit

--- slices/productApi.js
+++ slices/productApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productApi = createApi({
 	reducerPath: "productApi",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "https://shopapi.mahdisportfolio.ir/allProducts/",
+		baseUrl: "https://shopapi.mahdisportfolio.ir/",
 	}),
 	endpoints: (builder) => ({
 		getAllProducts: builder.query({
